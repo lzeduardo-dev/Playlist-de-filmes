29dee7af549ea427335fcebc4d287473c52364f7
Luiz Eduardo <luizeduardomgm81@gmail.com>
2025-05-31 20:57:40 -0300

    feat: capturando as entradas do usuário

--- playlist-filmes/src/script.js
+++ playlist-filmes/src/script.js
@@ -1,8 +1,12 @@
 const searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
+const movieName = document.getElementById("movie-name");
+const movieYear = document.getElementById("movie-year")
 
 function searchButtonClickHandler() {
   overlay.classList.add("open");
+  console.log(movieName.value.split(' ').join("+"))// tratamento para capturar o nome do filme
+  console.log("Ano: ", movieYear.value) // verificando captura do valor do ano
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
