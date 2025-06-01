5988ab1baeb6c6fd22f4bfb1a0074303790aa324
Luiz Eduardo <luizeduardomgm81@gmail.com>
2025-05-31 22:10:00 -0300

    feat: consumindo a api do OMDB

--- playlist-filmes/src/script.js
+++ playlist-filmes/src/script.js
@@ -1,12 +1,17 @@
 const searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
 const movieName = document.getElementById("movie-name");
-const movieYear = document.getElementById("movie-year")
+const movieYear = document.getElementById("movie-year");
+const key = "cedda5d9";
 
-function searchButtonClickHandler() {
+async function searchButtonClickHandler() {
+  let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName.value
+    .split(" ")
+    .join("+")}&y=${movieYear.value}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("data: ", data);  
   overlay.classList.add("open");
-  console.log(movieName.value.split(' ').join("+"))// tratamento para capturar o nome do filme
-  console.log("Ano: ", movieYear.value) // verificando captura do valor do ano
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
